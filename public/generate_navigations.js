ec575f2066fbe2e793f55e4b3dc30cc05fb73c16
Ayush Verma <vermajiii8116@gmail.com>
2026-03-04 12:26:30 +0530

    removes all key in separate pokemons * generates navigations separately for all and other types

--- public/generate_navigations.js
+++ public/generate_navigations.js
@@ -1,44 +1,41 @@
 const separatePokemons = (pokemons) => {
-  const all = []
-  const pokeTypes = {all : []}
   return pokemons.reduce((pokeTypes, pokemon) => {
-    all.push(pokemon);
     pokemon.types.forEach((type) => {
-      if (pokeTypes[type]) {
-        pokeTypes[type].push(pokemon);
-        return;
-      }
-      pokeTypes[type] = [];
+      pokeTypes[type] ||= [];
       pokeTypes[type].push(pokemon);
-      return;
     });
 
-    pokeTypes.all = all
-
     return pokeTypes;
-  }, pokeTypes);
+  }, {});
 };
 
 const format = (string) => {
   return string[0].toUpperCase() + string.slice(1);
-}
+};
 
-const generateNavigation = (type) => {
+const generateNavigation = (type, href) => {
   const anchor = document.createElement("a");
-  anchor.href = `${type === "all" ? "index" : type}.html`
+  anchor.href = href;
 
   const list = document.createElement("li");
+
   list.id = type;
   list.innerText = format(type);
   anchor.appendChild(list);
 
-  return anchor
-}
+  return anchor;
+};
 
 export const generateNavigations = (pokemons) => {
   const pokeTypes = separatePokemons(pokemons);
   console.log(pokeTypes);
+  
+  const allLink = generateNavigation("all", "index.html");
 
-  const navigations = Object.keys(pokeTypes).map(generateNavigation);
-  return navigations
-}
+  const navigations = Object.keys(pokeTypes).map((type) => {
+    const href = `${type}.html`;
+    return generateNavigation(type, href);
+  });
+
+  return [allLink, ...navigations];
+};
